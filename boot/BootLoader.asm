8b03dfcf77601c03aa34bcd94a4ac66508e07496
Yongwoon Lim <dev.chadlim@gmail.com>
2017-11-15 13:33:06 +0900

    Entering 32bit mode

---
+++ boot/BootLoader.asm
@@ -0,0 +1,167 @@
+[ORG 0x00]
+[BITS 16]
+
+SECTION .text
+
+jmp 0x7c0:START
+
+TOTAL_SECTOR_COUNT:		dw 0x01
+
+START:
+	mov ax, 0x07C0
+	mov ds, ax
+	mov ax, 0xb800
+	mov es, ax
+
+	mov ax, 0x0000
+	mov ss, ax
+	mov sp, 0xFFFE
+	mov bp, 0xFFFE
+
+	mov si, 0
+
+.SCREEN_CLEAR_LOOP:
+	mov byte [es:si], 0
+	mov byte [es:si+1], 0x0A
+
+	add si, 2
+
+	cmp si, 80 * 25 * 2
+	jl .SCREEN_CLEAR_LOOP
+
+	push BOOTLOADER_START_MESSAGE
+	push 0
+	push 0
+	call PRINT_MESSAGE
+	add sp, 6
+
+	push START_LOADING_IMAGE_MESSAGE
+	push 1
+	push 0
+	call PRINT_MESSAGE
+	add sp, 6
+
+RESET_DISK:
+	mov ax, 0
+	mov dl, 0
+	int 0x13
+	jc DISK_ERROR_HANDLER
+
+	mov si, 0x1000
+	mov es, si
+	mov bx, 0x0000
+
+	mov di, word [TOTAL_SECTOR_COUNT]
+
+READ_DATA:
+	cmp di, 0
+	je READ_END
+	sub di, 0x1
+	
+	mov ah, 0x02
+	mov al, 0x1
+	mov ch, byte [TRACK_NUMBER]
+	mov cl, byte [SECTOR_NUMBER]
+	mov dh, byte [HEAD_NUMBER]
+	mov dl, 0x00
+	int 0x13
+	jc DISK_ERROR_HANDLER
+
+	add si, 0x0020
+	mov es, si
+
+	mov al, byte [SECTOR_NUMBER]
+	add al, 0x01
+	mov byte [SECTOR_NUMBER], al
+	cmp al, 19
+	jl READ_DATA
+
+	xor byte [HEAD_NUMBER], 0x01
+	mov byte [SECTOR_NUMBER], 0x01
+
+	cmp byte [HEAD_NUMBER], 0x00
+	jne READ_DATA
+
+	add byte [TRACK_NUMBER], 0x01
+	jmp READ_DATA
+
+READ_END:
+	push IMAGE_LOADING_COMPLETE_MESSAGE
+	push 1
+	push 20
+	call PRINT_MESSAGE
+	add sp, 6
+
+	jmp 0x1000:0x0000
+
+DISK_ERROR_HANDLER:
+	push DISK_ERROR_MESSAGE
+	push 1
+	push 20
+	call PRINT_MESSAGE
+	
+	jmp $
+
+PRINT_MESSAGE:
+	push bp
+	mov bp, sp
+
+	push es
+	push si
+	push di
+	push ax
+	push cx
+	push dx
+
+	mov ax, 0xB800
+
+	mov es, ax
+
+	mov ax, word [bp + 6]
+	mov si, 160
+	mul si
+	mov di, ax
+
+	mov ax, word [bp + 4]
+	mov si, 2
+	mul si
+	add di, ax
+
+	mov si, word [bp + 8]
+
+.MESSAGE_LOOP:
+	mov cl, byte [si]
+
+	cmp cl, 0
+	je .MESSAGE_END
+	
+	mov byte [es:di], cl
+
+	add si, 1
+	add di, 2
+
+	jmp .MESSAGE_LOOP
+
+.MESSAGE_END:
+	pop dx
+	pop cx
+	pop ax
+	pop di
+	pop si
+	pop es
+	pop bp
+	ret
+
+BOOTLOADER_START_MESSAGE:		db 'Boot Loader Start', 0
+
+DISK_ERROR_MESSAGE:				db '!! Disk Error !!', 0
+START_LOADING_IMAGE_MESSAGE:	db 'OS Image Loading...', 0
+IMAGE_LOADING_COMPLETE_MESSAGE:	db 'Image Loading Complete', 0
+
+SECTOR_NUMBER:					db 0x02
+HEAD_NUMBER:					db 0x00
+TRACK_NUMBER:					db 0x00
+
+times 510 - ($ - $$) db 0x00
+
+dw 0xAA55
---
+++ makefile
@@ -0,0 +1,42 @@
+all: BootLoader Kernel32 Disk.img
+
+BootLoader:
+	@echo 
+	@echo ============== Build Boot Loader ============
+	@echo
+	
+	make -C boot
+
+	@echo 
+	@echo ============== Build Complete ============
+	@echo
+
+Kernel32:
+	@echo
+	@echo ============== Build 32bit kernel ============
+	@echo
+	
+	make -C kernel32
+
+	@echo
+	@echo ============== Build Complete ============
+	@echo
+
+Disk.img: boot/BootLoader.bin kernel32/Kernel32.bin
+	@echo
+	@echo ============== Disk Image Build ============
+	@echo
+
+	cat $^ > Disk.img
+
+	@echo
+	@echo ============== All Build Complete ============
+	@echo
+
+run: all
+	qemu-system-i386 -L . -fda Disk.img -localtime -M pc
+
+clean:
+	make -C boot clean
+	make -C kernel clean
+	rm -f Disk.img
